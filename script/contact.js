083ddb45e1c88ce06258874228ab108788e7b351
Zeki <zekixfly@hotmail.com>
2025-03-30 16:36:26 +0800

    Modify contact page

--- script/contact.js
+++ script/contact.js
@@ -1,7 +1,7 @@
 function isEmail(email) {
     var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     if (!regex.test(email)) {
-        getClasses("formTip")[0].innerHTML = "請填寫正確的郵件格式！";
+        getClasses("formTip")[0].innerHTML = "Please fill in the correct email format!";
         
     } else {
         getClasses("formTip")[0].innerHTML = "";
