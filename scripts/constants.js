f6f07c107f681cfcb593341cb054702b629d753d
Zeki <zekixfly@hotmail.com>
2026-01-31 22:29:01 +0800

    Fix All img's relative path again.

--- scripts/constants.js
+++ scripts/constants.js
@@ -2,5 +2,5 @@ Object.defineProperty(window, "API_GET_DATA", {
     configurable: false,
     writable: false,
     enumerable: true,
-    value: "../jsondata/pixelartwork.json",
+    value: "jsondata/pixelartwork.json",
 });
